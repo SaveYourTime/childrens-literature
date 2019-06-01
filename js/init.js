ca4751131a1efb266045eb1c6ce4356b3465d52c
Aaron Lu <aaron_lu1@yahoo.com>
2019-06-01 17:19:02 +0800

    add nav transition effect & fixed overlay above nav in mobile & add scrolling effect

--- js/init.js
+++ js/init.js
@@ -15,7 +15,13 @@
 
     document.querySelector('.read-more a').addEventListener('click', (e) => {
       document.querySelector('.read-more').style.display = 'none';
-      document.querySelector('.table-wrapper').classList.add('show');
+      document.querySelector('.table-wrapper').style.maxHeight = `${document.querySelector('.table-wrapper').scrollHeight}px`;
+    });
+
+    window.addEventListener('scroll', () => {
+      const nav = document.querySelector('nav');
+      nav.classList.toggle('hide-box-shadow', !window.pageYOffset);
+      window.innerWidth > 992 && nav.style.setProperty('background-color', `rgba(255, 255, 255, ${window.pageYOffset / 300})`, 'important');
     });
   });
 })(jQuery);
