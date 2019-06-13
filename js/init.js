75faa9f81cc4718a407c290b9f56475222887e41
Aaron Lu <aaron_lu1@yahoo.com>
2019-06-14 05:51:59 +0800

    Change scroll-behavior from css to js and using polyfill

--- js/init.js
+++ js/init.js
@@ -26,8 +26,18 @@
 
     window.addEventListener('scroll', () => {
       const nav = document.querySelector('nav');
-      nav.classList.toggle('hide-box-shadow', !window.pageYOffset);
+      nav.classList.toggle('hide-box-shadow', window.pageYOffset > 0 ? false : true);
       window.innerWidth > 992 && nav.style.setProperty('background-color', `rgba(255, 255, 255, ${window.pageYOffset / 300})`, 'important');
+    });
+
+    const navs = [...document.querySelectorAll('.navbar-fixed ul li a'), ...document.querySelectorAll('.sidenav li a')];
+    navs.forEach((nav) => {
+      nav.addEventListener('click', function (e) {
+        e.preventDefault();
+        const elementId = this.getAttribute('href');
+        const top = elementId === '#' ? 0 : document.querySelector(elementId).offsetTop - 64;
+        window.scroll({ top, left: 0, behavior: 'smooth' });
+      });
     });
   });
 })(jQuery);
