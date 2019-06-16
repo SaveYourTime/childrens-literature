1a899a353dd84f7e87b75a7918771746b5bc07fc
Aaron Lu <aaron_lu1@yahoo.com>
2019-06-16 22:22:45 +0800

    Add ga

--- js/init.js
+++ js/init.js
@@ -19,25 +19,26 @@
       interval: 2000,
     });
 
-    document.querySelector('.read-more a').addEventListener('click', (e) => {
+    document.querySelector('.read-more a').addEventListener('click', () => {
       document.querySelector('.read-more').style.display = 'none';
       document.querySelector('.table-wrapper').style.maxHeight = `${document.querySelector('.table-wrapper').scrollHeight}px`;
     });
 
-    window.addEventListener('scroll', () => {
+    const headerScrollTransition = () => {
       const nav = document.querySelector('nav');
       nav.classList.toggle('hide-box-shadow', window.pageYOffset > 0 ? false : true);
       window.innerWidth > 992 && nav.style.setProperty('background-color', `rgba(255, 255, 255, ${window.pageYOffset / 300})`, 'important');
-    });
+    }
+    window.addEventListener('scroll', headerScrollTransition);
 
     const navs = [...document.querySelectorAll('.navbar-fixed ul li a'), ...document.querySelectorAll('.sidenav li a')];
-    navs.forEach((nav) => {
-      nav.addEventListener('click', function (e) {
-        e.preventDefault();
-        const elementId = this.getAttribute('href');
-        const top = elementId === '#' ? 0 : document.querySelector(elementId).offsetTop - 64;
-        window.scroll({ top, left: 0, behavior: 'smooth' });
-      });
-    });
+    function bindScrollEffect(e) {
+      e.preventDefault();
+      const elementId = this.getAttribute('href');
+      const top = elementId === '#' ? 0 : document.querySelector(elementId).offsetTop - 64;
+      window.scroll({ top, left: 0, behavior: 'smooth' });
+      gtag('event', 'nav', { 'type': this.textContent });
+    }
+    navs.forEach((nav) => nav.addEventListener('click', bindScrollEffect));
   });
 })(jQuery);
