155555d8302fcf4db6f4e0ece1904e0395721c67
Aaron Lu <aaron_lu1@yahoo.com>
2019-07-01 01:51:00 +0800

    Add lazy loading

--- js/init.js
+++ js/init.js
@@ -1,5 +1,19 @@
 (function ($) {
   $(function () {
+    if ('loading' in HTMLImageElement.prototype) {
+      const images = document.querySelectorAll("img.lazyload");
+      images.forEach(img => {
+        img.src = img.dataset.src;
+      });
+    } else {
+      // Dynamically import the LazySizes library
+      let script = document.createElement("script");
+      script.async = true;
+      script.src =
+        "https://cdnjs.cloudflare.com/ajax/libs/lazysizes/4.1.8/lazysizes.min.js";
+      document.body.appendChild(script);
+    }
+
     M.AutoInit();
 
     $('.sidenav')
